001ab761574d4347b26f99b4558b99f87a59ce9d
Andrzej Litkowski <lank891@gmail.com>
2021-03-15 10:35:46 +0100

    Removed empty model, repository, service and controller and tests

--- Backend/src/main/java/se/backend/dao/EmptyRepository.java
+++
@@ -1,9 +0,0 @@
-package se.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import se.backend.model.EmptyModel;
-
-public interface EmptyRepository extends JpaRepository<EmptyModel, Long>, JpaSpecificationExecutor<EmptyModel> {
-
-}
