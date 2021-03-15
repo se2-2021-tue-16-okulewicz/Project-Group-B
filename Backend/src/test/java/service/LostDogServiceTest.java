8d7aa2faa09aa57cffc1cf938a5b598fe06bfa1e
Andrzej Litkowski <lank891@gmail.com>
2021-03-15 17:32:45 +0100

    Updated tests for changes in model and services

--- Backend/src/test/java/service/LostDogServiceTest.java
+++ Backend/src/test/java/service/LostDogServiceTest.java
@@ -8,8 +8,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.DirtiesContext;
 import se.backend.SEBackend;
+import se.backend.model.Picture;
 import se.backend.model.dogs.LostDog;
 import se.backend.service.lostdogs.LostDogService;
+import se.backend.wrapper.dogs.LostDogWithBehaviors;
+import se.backend.wrapper.dogs.LostDogWithBehaviorsAndPicture;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,13 +26,13 @@ public class LostDogServiceTest {
    private LostDogService service;
 
    private static Specification<LostDog> isFromLublin() {
-       return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("lostLocation"), "Lublin");
+       return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("location").get("city"), "Lublin");
    }
 
     private static Specification<LostDog> isFromWarsaw() {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("lostLocation"), "Warsaw");
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("location").get("city"), "Warsaw");
     }
-/*
+
     @Test
     public void AddLostDogTest() {
 
@@ -39,29 +42,58 @@ public class LostDogServiceTest {
 
         //Adding dogs
         LostDog newDog1 = new LostDog();
-        newDog1.setLostLocation("Lublin");
+        newDog1.setName("Name1");
+        LostDogWithBehaviors newDogBeh1 = new LostDogWithBehaviors(newDog1);
+        Picture pic1 = new Picture();
+        pic1.setFileName("exampleFile1");
 
         LostDog newDog2 = new LostDog();
-        newDog2.setLostLocation("Lublin");
+        newDog2.setName("Name2");
+        LostDogWithBehaviors newDogBeh2 = new LostDogWithBehaviors(newDog2);
+        newDogBeh2.getBehaviors().add("Barks");
+        Picture pic2 = new Picture();
+        pic2.setFileName("exampleFile2");
 
         LostDog newDog3 = new LostDog();
-        newDog3.setLostLocation("Warsaw");
+        newDog3.setName("Name3");
+        LostDogWithBehaviors newDogBeh3 = new LostDogWithBehaviors(newDog3);
+        newDogBeh3.getBehaviors().add("Wags tail");
+        newDogBeh3.getBehaviors().add("Drinks milk");
+        Picture pic3 = new Picture();
+        pic3.setFileName("exampleFile3");
 
-        var result1 = service.AddLostDog(newDog1);
-        var result2 = service.AddLostDog(newDog2);
-        var result3 = service.AddLostDog(newDog3);
+        LostDogWithBehaviorsAndPicture dogAndPicture1 = new LostDogWithBehaviorsAndPicture();
+        dogAndPicture1.setDog(newDogBeh1);
+        dogAndPicture1.setPicture(pic1);
+        LostDogWithBehaviorsAndPicture dogAndPicture2 = new LostDogWithBehaviorsAndPicture();
+        dogAndPicture2.setDog(newDogBeh2);
+        dogAndPicture2.setPicture(pic2);
+        LostDogWithBehaviorsAndPicture dogAndPicture3 = new LostDogWithBehaviorsAndPicture();
+        dogAndPicture3.setDog(newDogBeh3);
+        dogAndPicture3.setPicture(pic3);
+
+        var result1 = service.AddLostDog(dogAndPicture1);
+        var result2 = service.AddLostDog(dogAndPicture2);
+        var result3 = service.AddLostDog(dogAndPicture3);
 
         //Checking results of adding
-        assertEquals("Lublin", result1.getLostLocation());
-        assertEquals("Lublin", result2.getLostLocation());
-        assertEquals("Warsaw", result3.getLostLocation());
+        assertEquals("Name1", result1.getName());
+        assertEquals(0, result1.getBehaviors().size());
+        assertEquals("exampleFile1", result1.getPicture().getFileName());
+
+        assertEquals("Name2", result2.getName());
+        assertEquals(1, result2.getBehaviors().size());
+        assertEquals("exampleFile2", result2.getPicture().getFileName());
+
+        assertEquals("Name3", result3.getName());
+        assertEquals(2, result3.getBehaviors().size());
+        assertEquals("exampleFile3", result3.getPicture().getFileName());
 
         //Getting all dogs again
         allDogs = service.GetLostDogs(Specification.where(null), PageRequest.of(0, 15));
         assertEquals(allDogs.size(), 7);
     }
-*/
-/*
+
     @Test
     public void FilteredDogsTests() {
         var lublinDogs = service.GetLostDogs(Specification.where(isFromLublin()), PageRequest.of(0, 15));
@@ -72,5 +104,4 @@ public class LostDogServiceTest {
         assertEquals(1, warsawDogs.size());
         assertEquals(4, allDogsWithOr.size());
     }
- */
 }
--- Backend/src/test/resources/data.sql
+++ Backend/src/test/resources/data.sql
@@ -1,6 +1,22 @@
 --hibernate will start sequence with id = 1, so use high ids to make sure, that hibernate won't break unique id
 
-insert into lost_dogs (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, lost_location, user_id, is_found) values (10001, 12, 'unknown', 'blue', 'long', 'long', 'Pinky', 'big', 'none', 'long', null, 'Lublin', 10001, 0);
-insert into lost_dogs (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, lost_location, user_id, is_found) values (10002, 13, 'random', 'brown', 'long', 'short', 'Borys', 'big', 'none', 'short', null, 'Lublin', 10001, 1);
-insert into lost_dogs (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, lost_location, user_id, is_found) values (10003, 14, 'bulldog', 'blonde', 'long', 'short', 'Warus', 'small', 'gray ears', 'long', null, 'Lublin', 10002, 0);
-insert into lost_dogs (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, lost_location, user_id, is_found) values (10004, 15, 'unknown', 'pink', 'short', 'long', 'Pimpek', 'small', 'none', 'long', null, 'Warsaw', 10003, 1);
+insert into lost_dog (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, city, owner_id, is_is_found, district, picture_id) values (10001, 12, 'unknown', 'blue', 'long', 'long', 'Pinky', 'big', 'none', 'long', null, 'Lublin', 10001, 0, 'Bronowice', 10001);
+insert into lost_dog (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, city, owner_id, is_is_found, district, picture_id) values (10002, 13, 'random', 'brown', 'long', 'short', 'Borys', 'big', 'none', 'short', null, 'Lublin', 10001, 1, 'Bronowice', 10002);
+insert into lost_dog (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, city, owner_id, is_is_found, district, picture_id) values (10003, 14, 'bulldog', 'blonde', 'long', 'short', 'Warus', 'small', 'gray ears', 'long', null, 'Lublin', 10002, 0, 'LSM', 10003);
+insert into lost_dog (id, age, breed, color, ears_type, hair_length, name, size, special_marks, tail_length, date_lost, city, owner_id, is_is_found, district, picture_id) values (10004, 15, 'unknown', 'pink', 'short', 'long', 'Pimpek', 'small', 'none', 'long', null, 'Warsaw', 10003, 1, 'Ursynów', 10004);
+
+insert into picture (id, file_name, file_type, data) values (10001, 'example1', 'text/plain', RAWTOHEX('example_data1'));
+insert into picture (id, file_name, file_type, data) values (10002, 'example2', 'text/plain', RAWTOHEX('example_data2'));
+insert into picture (id, file_name, file_type, data) values (10003, 'example3', 'text/plain', RAWTOHEX('example_data3'));
+insert into picture (id, file_name, file_type, data) values (10004, 'example4', 'text/plain', RAWTOHEX('example_data4'));
+
+
+insert into dog_behavior (id, dog_id, behavior) values (10001, 10001, 'Barks');
+insert into dog_behavior (id, dog_id, behavior) values (10002, 10001, 'Wags tail');
+insert into dog_behavior (id, dog_id, behavior) values (10003, 10001, 'Eats');
+insert into dog_behavior (id, dog_id, behavior) values (10004, 10002, 'Wants to conquer the world');
+insert into dog_behavior (id, dog_id, behavior) values (10005, 10003, 'Drinks milk');
+insert into dog_behavior (id, dog_id, behavior) values (10006, 10003, 'Sprints very fast');
+insert into dog_behavior (id, dog_id, behavior) values (10007, 10004, 'Barks');
+insert into dog_behavior (id, dog_id, behavior) values (10008, 10004, 'Drinks milk');
+insert into dog_behavior (id, dog_id, behavior) values (10009, 10004, 'Eats');
