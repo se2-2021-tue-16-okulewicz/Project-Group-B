52b3246987ed22a2bd9d6e90aeb9c98f0195d81c
Polina Brazhko <polina.brazhko@gmail.com>
2021-03-20 13:10:54 +0100

    Authentication in controller + test

---
+++ Backend/src/main/java/se/backend/controller/internal/UserController.java
@@ -0,0 +1,45 @@
+package se.backend.controller.internal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.backend.dao.UserAccountRepository;
+import se.backend.exceptions.types.GenericBadRequestException;
+import se.backend.model.account.Account;
+import se.backend.model.account.UserAccount;
+import se.backend.service.login.LoginService;
+import se.backend.service.lostdogs.LostDogService;
+import se.backend.utils.Response;
+import se.backend.wrapper.account.AuthenticationResults;
+
+import java.net.http.HttpResponse;
+
+@RestController
+@RequestMapping(path = "/account")
+public class UserController {
+    private UserAccountRepository userAccountRepository;
+    private final LoginService loginService;
+
+    @Autowired
+    public UserController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    public ResponseEntity<Response<UserAccount>> createUserAccount(UserAccount user, String password){
+        return null;
+    }
+    public ResponseEntity<Response<UserAccount>> updateUser(UserAccount user) {
+        return null;
+    }
+    @PostMapping(path = "/login")
+    public ResponseEntity<Response<AuthenticationResults>> authenticate(String username, String password){
+        var result = this.loginService.authenticate(username,password);
+        if(result == null){
+            throw new GenericBadRequestException("Invalid credentials");
+        }
+        return ResponseEntity.ok(new Response<>(String.format("Token: %s", result.getToken()), true, result));
+    }
+
+}
---
+++ Backend/src/test/java/controller/internal/UserControllerTest.java
@@ -0,0 +1,41 @@
+package controller.internal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import se.backend.SEBackend;
+import se.backend.wrapper.account.LoginInfo;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = SEBackend.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void authenticateTest() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(new LoginInfo("e.musk@mail.com", "xea-12Musk"));
+        System.out.println(json);
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/account/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk());
+    }
+}
