90731737a219e8b4f023143e6dc69b556725fc21
Lank891 <Lank891@users.noreply.github.com>
2021-03-23 01:31:36 +0000

    Code prettified

--- webapp/src/registerDog/ImageUpload.test.tsx
+++ webapp/src/registerDog/ImageUpload.test.tsx
@@ -2,33 +2,32 @@
  * @jest-environment jsdom
  */
 
- import React from "react";
- import { render, unmountComponentAtNode } from "react-dom";
- import { act } from "react-dom/test-utils";
- import ImageUpload from "./ImageUpload";
- import { isNull } from "lodash";
- 
- let container : HTMLDivElement | null = null;
- beforeEach(() => {
-   // setup a DOM element as a render target
-   container = document.createElement("div");
-   document.body.appendChild(container);
- });
- 
- afterEach(() => {
-   // cleanup on exiting
-   if(!isNull(container)) {
-     unmountComponentAtNode(container);
-     container.remove();
-     container = null;
-   }
- });
- 
- it("Render upload image part of dog register", () => {
-   act(() => {
-     render(<ImageUpload/>, container);
-   });
- 
-   expect(container?.getElementsByClassName("imageContainer").length).toBe(1);
- });
- 
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import ImageUpload from "./ImageUpload";
+import { isNull } from "lodash";
+
+let container: HTMLDivElement | null = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  if (!isNull(container)) {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  }
+});
+
+it("Render upload image part of dog register", () => {
+  act(() => {
+    render(<ImageUpload />, container);
+  });
+
+  expect(container?.getElementsByClassName("imageContainer").length).toBe(1);
+});
--- webapp/src/registerDog/registerDog.test.tsx
+++ webapp/src/registerDog/registerDog.test.tsx
@@ -2,33 +2,34 @@
  * @jest-environment jsdom
  */
 
- import React from "react";
- import { render, unmountComponentAtNode } from "react-dom";
- import { act } from "react-dom/test-utils";
- import RegisterDogForm from "./registerDog";
- import { isNull } from "lodash";
- 
- let container : HTMLDivElement | null = null;
- beforeEach(() => {
-   // setup a DOM element as a render target
-   container = document.createElement("div");
-   document.body.appendChild(container);
- });
- 
- afterEach(() => {
-   // cleanup on exiting
-   if(!isNull(container)) {
-     unmountComponentAtNode(container);
-     container.remove();
-     container = null;
-   }
- });
- 
- it("Render form for dog register", () => {
-   act(() => {
-     render(<RegisterDogForm/>, container);
-   });
- 
-   expect(container?.getElementsByClassName("MuiGrid-root").length).toBeGreaterThan(0);
- });
- 
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import RegisterDogForm from "./registerDog";
+import { isNull } from "lodash";
+
+let container: HTMLDivElement | null = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  if (!isNull(container)) {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  }
+});
+
+it("Render form for dog register", () => {
+  act(() => {
+    render(<RegisterDogForm />, container);
+  });
+
+  expect(
+    container?.getElementsByClassName("MuiGrid-root").length
+  ).toBeGreaterThan(0);
+});
