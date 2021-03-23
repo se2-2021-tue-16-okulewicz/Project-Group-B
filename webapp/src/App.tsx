37f744ef87ee1bffb905062116e42f9c8ad6c772
Andrzej Litkowski <lank891@gmail.com>
2021-03-23 05:37:38 +0100

    Basic login design

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import RegisterDogForm from "./registerDog/registerDog";
+import Login from "./registerLogin/Login";
 
 function App() {
   return (
     <div>
-      <RegisterDogForm />
+      <Login />
     </div>
   );
 }
