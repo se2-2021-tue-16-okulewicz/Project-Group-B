736f0f3c13fdfc9abb5cc8c0335ccc139d59309c
Andrzej Litkowski <lank891@gmail.com>
2021-03-28 14:00:03 +0200

    Authorization tests

--- Backend/src/test/java/service/LoginServiceTest.java
+++ Backend/src/test/java/service/LoginServiceTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.annotation.DirtiesContext;
 import se.backend.SEBackend;
 import se.backend.service.login.LoginService;
 import se.backend.wrapper.account.UserType;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -35,5 +39,47 @@ public class LoginServiceTest {
 
         var failedResults = service.Authenticate("not-existing-acc", "not-existing-pwd");
         assertNull(failedResults);
+    }
+
+    @Test
+    public void isAuthorizedTest() {
+        HttpHeaders regularTokenHeaders = new HttpHeaders();
+        regularTokenHeaders.add("token", "regularUserTestToken");
+
+        HttpHeaders adminTokenHeaders = new HttpHeaders();
+        adminTokenHeaders.add("token", "testTokenForAdmins");
+
+        HttpHeaders shelterTokenHeaders = new HttpHeaders();
+        shelterTokenHeaders.add("token", "shelterSecretTestToken");
+
+        HttpHeaders invalidTokenHeaders = new HttpHeaders();
+        invalidTokenHeaders.add("token", "i am invalid token");
+
+        HttpHeaders noTokenHeaders = new HttpHeaders();
+
+        var res1 = service.IsAuthorized(regularTokenHeaders, Collections.singletonList(UserType.Regular));
+        assertEquals(true, res1.getValue0());
+        assertEquals(10001, res1.getValue1());
+
+        var res2 = service.IsAuthorized(regularTokenHeaders, List.of(UserType.Admin, UserType.Regular, UserType.Shelter));
+        assertEquals(true, res2.getValue0());
+        assertEquals(10001, res2.getValue1());
+
+        var res3 = service.IsAuthorized(regularTokenHeaders, Collections.singletonList(UserType.Admin));
+        assertEquals(false, res3.getValue0());
+
+        var res4 = service.IsAuthorized(adminTokenHeaders, Collections.singletonList(UserType.Admin));
+        assertEquals(true, res4.getValue0());
+        assertEquals(10001, res4.getValue1());
+
+        var res5 = service.IsAuthorized(shelterTokenHeaders, Collections.singletonList(UserType.Shelter));
+        assertEquals(true, res5.getValue0());
+        assertEquals(10001, res5.getValue1());
+
+        var res6 = service.IsAuthorized(invalidTokenHeaders, List.of(UserType.Admin, UserType.Regular, UserType.Shelter));
+        assertEquals(false, res6.getValue0());
+
+        var res7 = service.IsAuthorized(noTokenHeaders, List.of(UserType.Admin, UserType.Regular, UserType.Shelter));
+        assertEquals(false, res7.getValue0());
     }
 }
