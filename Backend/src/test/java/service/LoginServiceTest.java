8bfe5c0feabd230ec168f0e3b2e644eb54998628
Andrzej Litkowski <lank891@gmail.com>
2021-03-28 14:01:28 +0200

    Changed order of arguments in existing tests Just to match argument names of testing functions

--- Backend/src/test/java/service/LoginServiceTest.java
+++ Backend/src/test/java/service/LoginServiceTest.java
@@ -26,16 +26,16 @@ public class LoginServiceTest {
     @Test
     public void authenticateTest() {
         var authenticationResult1 = service.Authenticate("e.musk@mail.com", "xea-12Musk");
-        assertEquals(authenticationResult1.getUserType(), UserType.Regular);
+        assertEquals(UserType.Regular, authenticationResult1.getUserType());
 
         var authenticationResult2 = service.Authenticate("b.gates@mail.com", "MicrosoftTheBest");
-        assertEquals(authenticationResult2.getUserType(),UserType.Regular);
+        assertEquals(UserType.Regular, authenticationResult2.getUserType());
 
         var authenticationResult3 = service.Authenticate("hopeShelter", "12345678");
-        assertEquals(authenticationResult3.getUserType(),UserType.Shelter);
+        assertEquals(UserType.Shelter, authenticationResult3.getUserType());
 
         var authenticationResult4 = service.Authenticate("admin007", "admin007123");
-        assertEquals(authenticationResult4.getUserType(),UserType.Admin);
+        assertEquals(UserType.Admin, authenticationResult4.getUserType());
 
         var failedResults = service.Authenticate("not-existing-acc", "not-existing-pwd");
         assertNull(failedResults);
