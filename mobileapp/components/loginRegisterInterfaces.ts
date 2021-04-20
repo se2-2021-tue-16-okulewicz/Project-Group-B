fbf2e33606d770fb5c4dd611fb163ead43a69844
Polina Brazhko <polina.brazhko@gmail.com>
2021-04-20 20:35:23 +0200

    receiving token

--- mobileapp/components/loginRegisterInterfaces.ts
+++ mobileapp/components/loginRegisterInterfaces.ts
@@ -5,6 +5,6 @@ export interface ILoginInformation {
 
 export interface ILoginResults {
   userType: "Admin" | "Regular" | "Shelter" | "";
-  Authorization: string;
+  token: string;
   id: number;
 }
