db2e5a24559b3f98cc78f7e81bac1fe39e91f7a8
Polina Brazhko <polina.brazhko@gmail.com>
2021-04-20 15:35:41 +0200

    register screen

---
+++ mobileapp/components/register-login/Register.tsx
@@ -0,0 +1,216 @@
+import { StyleSheet, Text, SafeAreaView, View, KeyboardAvoidingView, Platform, Modal, Pressable, Image } from 'react-native';
+import * as React from "react";
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { store } from '../../redux/store';
+import * as Actions from '../../redux/actions';
+import SignIn from './SignIn';
+import * as Utility from '../../redux/utility.js';
+import { useSelector } from "react-redux";
+import * as styles from "../../constants/account";
+import { State } from '../../redux/reducer';
+
+
+const Register = ({ navigation }: any) => {
+  //need to add onSubmit function above, I commented it out bcs it was not working
+  const [userName, setUserName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [ready, setReady] = React.useState(false);
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const errorMessage = useSelector((state: State) => state.error.erorMessage);
+  const status = useSelector((state: State) => state.status);
+
+  function isStringValidPassword(password: string): boolean {
+    return password.length <= 32 && password.length >= 6;
+  }
+
+  function isStringValidUsername(username: string): boolean {
+    return username.length <= 32 && username.length >= 3;
+  }
+
+  function isStringValidPhoneNumeber(phone: string): boolean {
+    let phoneNumberGarbage = new RegExp("[()\\s-]+", "g");
+    let phoneNumber = new RegExp("^((\\+[1-9]?[0-9])|0)?[7-9]?[0-9]{9}$");
+    if (phone === "") return false;
+    phone = phone.replace(phoneNumberGarbage, "");
+    return phoneNumber.test(phone);
+  }
+
+  function isStringValidEmail(email: string): boolean {
+    //Should catch like 99%+ of valid emails
+    let emailRegex: RegExp = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+    return emailRegex.test(email);
+  }
+
+  React.useEffect(() => {
+    if (errorMessage !== "") {
+      setModalVisible(true);
+    }
+  }, [errorMessage]);
+
+
+  async function register() {
+    store.dispatch(
+      Actions.registerRegularUserThunk({
+        username: userName,
+        password: password,
+        email: email,
+        phone: phoneNumber,
+      })
+    );
+  }
+
+  // React.useEffect(() => {
+  //   if (status == "redirectToDogs") {
+  //     navigation.push("DogsList");
+  //   }
+  // }, [status]);
+
+  // const status = useSelector((state) => state.status);
+  // React.useEffect(() => {
+  //   if (status === "redirectToLogin") {
+  //     setReady(false);
+  //     store.dispatch(Actions.setIdle());
+  //     navigation.pop();
+  //     return;
+  //   }
+  //   if (status === "registering") {
+  //     setReady(false);
+  //     return;
+  //   }
+  //   if (status === "registerError") {
+  //     setReady(true);
+  //     setModalVisible(true);
+  //   }
+  // }, [status])
+
+  React.useEffect(() => {
+    if (isStringValidEmail(email) && isStringValidUsername(userName) && isStringValidPhoneNumeber(phoneNumber) && isStringValidPassword(password)) {
+      setReady(true);
+      return;
+    }
+    setReady(false);
+  }, [email, password]);
+
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.styles.containerMain}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={style.centeredView}>
+          <View style={style.modalView}>
+            <Text style={style.modalText}>Registration failed!</Text>
+            <Text style={style.modalText}>{errorMessage}</Text>
+            <Pressable
+              style={[style.modalButton, style.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={style.textStyle}>OK</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <View>
+        <Image
+          style={style.tinyLogo}
+          source={require("../../assets/images/dog-paw.png")}
+        />
+      </View>
+      <View>
+        <TextInput placeholder="Username" style={styles.styles.textInput} onChangeText={(e) => setUserName(e)}></TextInput>
+        <TextInput placeholder="E-mail" style={styles.styles.textInput} onChangeText={(e) => setEmail(e)}></TextInput>
+        <TextInput placeholder="Phone number" style={styles.styles.textInput} onChangeText={(e) => setPhoneNumber(e)}></TextInput>
+        <TextInput maxLength={32} secureTextEntry={true} placeholder="Password" style={styles.styles.textInput} onChangeText={(e) => setPassword(e)}></TextInput>
+        <TouchableOpacity disabled={!ready} style={ready ? style.button : style.disabledButton} onPress={() => register()}>
+          <Text style={style.buttonText}>Register </Text>
+        </TouchableOpacity>
+      </View>
+      <SafeAreaView style={styles.styles.bottomView}>
+        <Text style={styles.styles.textStyle}>Already have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.pop()}>
+          <Text style={styles.styles.link}>Sign in</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const style = StyleSheet.create({
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    width: 280,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 25,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalButton: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    width: 120
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    color: 'red'
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#09bd09",
+    padding: 10,
+    borderRadius: 15,
+    width: 250,
+    margin: 7
+  },
+  disabledButton: {
+    alignItems: "center",
+    backgroundColor: "#9da2a6",
+    padding: 10,
+    borderRadius: 15,
+    width: 250,
+    margin: 7
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15
+  }
+})
+
+export default Register;
