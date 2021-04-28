99921381c65deebcf74c119020a4acc39f10a3d4
Polina Brazhko <polina.brazhko@gmail.com>
2021-04-28 18:21:15 +0200

    image uploader

--- mobileapp/screens/AddDogScreen.tsx
+++ mobileapp/screens/AddDogScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageUpload from "../components/helper/ImageUpload";
 
 export default class AddDogScreen extends React.Component { 
     render() { 
       return (  
           <View style={styles.container}>  
+          <ImageUpload></ImageUpload>
             <Text>Add dog Screen</Text>  
           </View>  
       );  
