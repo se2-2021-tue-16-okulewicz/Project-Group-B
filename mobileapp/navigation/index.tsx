ee5ee37995b3c6e69d6781aee4e94571bb24beac
Polina Brazhko <polina.brazhko@gmail.com>
2021-04-28 15:40:26 +0200

    app navigation

--- mobileapp/navigation/index.tsx
+++ mobileapp/navigation/index.tsx
@@ -2,40 +2,26 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
 import { ColorSchemeName } from "react-native";
-
-// import NotFoundScreen from "../screens/NotFoundScreen";
-// import { RootStackParamList } from "../types";
-import BottomTabNavigator from "./BottomTabNavigator";
-import LinkingConfiguration from "./LinkingConfiguration";
 import LogInRegister from "../components/register-login/LogInRegister";
-import DogsList from "../components/dogs/DogsList";
 import { RootStackParamList } from "../types";
-// If you are not familiar with React Navigation, we recommend going through the
-// "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
+import BottomTab from "./TabNavigator";
 export default function Navigation() {
   return (
-    <NavigationContainer linking={LinkingConfiguration}>
+    <NavigationContainer >
       <RootNavigator />
     </NavigationContainer>
   );
 }
-
-// A root stack navigator is often used for displaying modals on top of all other content
-// Read more here: https://reactnavigation.org/docs/modal
 const Stack = createStackNavigator<RootStackParamList>();
+
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }} >
       <Stack.Screen name="LogInRegister" component={LogInRegister} />
-      <Stack.Screen name="DogsList" component={DogsList} />
 
-      {/* <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen
-        name="NotFound"
-        component={NotFoundScreen}
-        options={{ title: "Oops!" }}
-      /> */}
+      <Stack.Screen name="Root" component={BottomTab} />
     </Stack.Navigator>
   );
 }
+
