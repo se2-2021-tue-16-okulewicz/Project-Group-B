3e3a7ab2e895d0dc9056d0354ee16f46b977f8ea
Polina Brazhko <polina.brazhko@gmail.com>
2021-04-28 15:40:55 +0200

    screen for navigation

---
+++ mobileapp/screens/DogsScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DogsList from "../components/dogs/DogsList";
+
+export default class DogsScreen extends React.Component{ 
+    render(){
+      return(  
+          <DogsList></DogsList>
+      );  
+    }
+  };
