ff540dedf5765beeb7573548bd622155fb30e959
Polina Brazhko <polina.brazhko@gmail.com>
2021-05-04 21:37:38 +0200

    image uploader

--- mobileapp/components/helper/ImageUpload.tsx
+++ mobileapp/components/helper/ImageUpload.tsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import { View, Text, Image, Button } from 'react-native'
-import ImagePicker from 'react-native-image-picker'
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { store } from '../../redux/store';
+import * as Actions from "../../redux/actions";
 
-export default class ImageUpload extends React.Component {
-  state = {
-    photo: null,
-  }
+export default function ImageUpload()  {
+  const [image, setImage] = useState("");
 
-  handleChoosePhoto = () => {
-    const options = {
-      noData: true,
-    }
-    ImagePicker.launchImageLibrary(options, response => {
-      if (response.uri) {
-        this.setState({ photo: response })
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
       }
-    })
-  }
+    })();
+  }, []);
 
-  render() {
-    const { photo } = this.state
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {photo && (
-          <Image
-            source={{ uri: photo.uri }}
-            style={{ width: 300, height: 300 }}
-          />
-        )}
-        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
-      </View>
-    )
-  }
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      store.dispatch(Actions.setImage(image));
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TouchableOpacity  onPress={() => pickImage()} >
+        <Text>Pick an image from camera roll</Text>
+      </TouchableOpacity >
+      {/* {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />} */}
+    {image ? <Image source={{ uri: image }} style={{ width: 400, height: 400 }} /> : < View/>}
+    </View>
+  );
 }
