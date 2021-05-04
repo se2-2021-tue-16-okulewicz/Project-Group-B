8aa2f8f7ae81630d4075e3dcde1b1cd238abc670
Polina Brazhko <polina.brazhko@gmail.com>
2021-05-04 21:39:35 +0200

    add dog screen is stack

--- mobileapp/screens/AddDogScreen.tsx
+++ mobileapp/screens/AddDogScreen.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ImageUpload from "../components/helper/ImageUpload";
+import AddDogStack from "./AddDogStack";
 
 export default class AddDogScreen extends React.Component { 
     render() { 
       return (  
-          <View style={styles.container}>  
-          <ImageUpload></ImageUpload>
-            <Text>Add dog Screen</Text>  
-          </View>  
+        <AddDogStack/>
+          // <View style={styles.container}>  
+          // <ImageUpload></ImageUpload>
+          //   <Text>Add dog Screen</Text>  
+          // </View>  
       );  
     }
 };
