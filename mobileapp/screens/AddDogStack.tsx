2c59a76322043d402eb446b8a4c9159b3d92fb50
Polina Brazhko <polina.brazhko@gmail.com>
2021-05-05 11:44:11 +0200

    dog stack header buttons

--- mobileapp/screens/AddDogStack.tsx
+++ mobileapp/screens/AddDogStack.tsx
@@ -4,14 +4,14 @@ import * as React from "react";
 import AddDetails from './AddDetails';
 import { TouchableOpacity, View } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faTimes, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { State } from '../redux/reducer';
 const inactiveColor="#919191";
 const activeColor="#006ee6";
 
 
-export default function MyStack() {
+export default function AddDogStack({ navigation }: any) {
     const Stack = createStackNavigator();
     const image = useSelector((state: State) => state.image);
     const [isImageChosen, setImageChosen] = React.useState(false);
@@ -31,11 +31,17 @@ export default function MyStack() {
           <Exit/>
         ),
         headerRight: () => (
-            <Next imageChosen={isImageChosen}/>
+            <Next imageChosen={isImageChosen} onPress={() => navigation.navigate("Add details")}/>
         )
       })}
       />
-      <Stack.Screen name="Add details" component={AddDetails} />
+      <Stack.Screen name="Add details" component={AddDetails} 
+      options={({navigation}) => ({
+        headerLeft: () => (
+            <Previous onPress={() => navigation.pop()}/>
+          )
+      })}
+      />
     </Stack.Navigator>
   );
 }
@@ -50,10 +56,20 @@ function Exit(){
     );
 }
 
+function Previous(props: any){
+    return(
+        <View>
+            <TouchableOpacity onPress={props.onPress}>
+                <FontAwesomeIcon icon={faChevronLeft} size={25} color={activeColor}></FontAwesomeIcon>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
 const Next = (props: any) => {
     return(
         <View>
-            <TouchableOpacity disabled={!props.imageChosen}>
+            <TouchableOpacity disabled={!props.imageChosen} onPress={props.onPress}>
                 <FontAwesomeIcon icon={faChevronRight} size={25} color={props.imageChosen ? activeColor : inactiveColor}></FontAwesomeIcon>
             </TouchableOpacity>
         </View>
