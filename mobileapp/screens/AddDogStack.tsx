f4f7f840c6b350a005dd0642b9b861d9ebc58c74
Polina Brazhko <polina.brazhko@gmail.com>
2021-05-14 17:32:42 +0200

    stack for different screens and header buttons handlers

--- mobileapp/screens/AddDogStack.tsx
+++ mobileapp/screens/AddDogStack.tsx
@@ -7,6 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faTimes, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { State } from '../redux/reducer';
+import { IDogCharacteristics } from '../components/dogs/dog/dogInterfaces';
+import { initLostDogCharacteristics } from '../components/dogs/dog/dogClasses';
 const inactiveColor="#919191";
 const activeColor="#006ee6";
 
@@ -14,12 +16,24 @@ const activeColor="#006ee6";
 export default function AddDogStack({ navigation }: any) {
     const Stack = createStackNavigator();
     const image = useSelector((state: State) => state.image);
+    const dogCharacteristics = useSelector((state: State) => state.dogCharacteristics);
     const [isImageChosen, setImageChosen] = React.useState(false);
+    const [isCharacteristicsInput, setCharacteristicsInput] = React.useState(false);
 
     React.useEffect(() => {
         setImageChosen(image !== "")
     },[image])
+
+    React.useEffect(() => {
+        setCharacteristicsInput(areValidCharacteristics(dogCharacteristics));
+    },[dogCharacteristics])
    
+    function areValidCharacteristics(ch: IDogCharacteristics){
+        return (ch.name != initLostDogCharacteristics.name && ch.breed != initLostDogCharacteristics.breed && ch.color != initLostDogCharacteristics.color
+          && ch.size != initLostDogCharacteristics.size && ch.hairLength != initLostDogCharacteristics.hairLength && ch.tailLength != initLostDogCharacteristics.hairLength 
+          && ch.tailLength != initLostDogCharacteristics.tailLength && ch.earsType != initLostDogCharacteristics.earsType && ch.specialMark != initLostDogCharacteristics.specialMark
+          && ch.behaviors != initLostDogCharacteristics.behaviors);
+      }
 
   return (
     <Stack.Navigator
@@ -31,15 +45,18 @@ export default function AddDogStack({ navigation }: any) {
           <Exit/>
         ),
         headerRight: () => (
-            <Next imageChosen={isImageChosen} onPress={() => navigation.navigate("Add details")}/>
+            <Next imageChosen={isImageChosen} onPress={() => navigation.navigate("Dog characteristics")}/>
         )
       })}
       />
-      <Stack.Screen name="Add details" component={AddDetails} 
+      <Stack.Screen name="Dog characteristics" component={AddDetails} 
       options={({navigation}) => ({
         headerLeft: () => (
             <Previous onPress={() => navigation.pop()}/>
-          )
+          ),
+          headerRight: () => (
+            <Next imageChosen={isCharacteristicsInput} onPress={() => navigation.navigate("Location and date")}/>
+        )
       })}
       />
     </Stack.Navigator>
@@ -67,9 +84,10 @@ function Previous(props: any){
 }
 
 const Next = (props: any) => {
+
     return(
         <View>
-            <TouchableOpacity disabled={!props.imageChosen} onPress={props.onPress}>
+            <TouchableOpacity disabled={props.isImageChosen} onPress={props.onPress}>
                 <FontAwesomeIcon icon={faChevronRight} size={25} color={props.imageChosen ? activeColor : inactiveColor}></FontAwesomeIcon>
             </TouchableOpacity>
         </View>
