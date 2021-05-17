9d20ca1c591cc31eabfa47065a95e848cc2bb9fc
Polina Brazhko <polina.brazhko@gmail.com>
2021-05-17 12:15:18 +0200

    stack for adding dog

--- mobileapp/screens/AddDogStack.tsx
+++ mobileapp/screens/AddDogStack.tsx
@@ -2,13 +2,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ImageUpload from '../components/helper/ImageUpload';
 import * as React from "react";
 import AddDetails from './AddDetails';
-import { TouchableOpacity, View } from 'react-native';
+import ReviewAndPost from './ReviewAndPost';
+import { TouchableOpacity, View, Text } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faTimes, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { State } from '../redux/reducer';
-import { IDogCharacteristics } from '../components/dogs/dog/dogInterfaces';
-import { initLostDogCharacteristics } from '../components/dogs/dog/dogClasses';
+import { IDogCharacteristics, IDogDetails, ILostDog } from '../components/dogs/dog/dogInterfaces';
+import { initDogDetails, initLostDogCharacteristics } from '../components/dogs/dog/dogClasses';
+import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
+import { store } from '../redux/store';
+import * as Actions from "../redux/actions";
 const inactiveColor="#919191";
 const activeColor="#006ee6";
 
@@ -16,9 +20,13 @@ const activeColor="#006ee6";
 export default function AddDogStack({ navigation }: any) {
     const Stack = createStackNavigator();
     const image = useSelector((state: State) => state.image);
+    const picture = useSelector((state: State) => state.picture);
     const dogCharacteristics = useSelector((state: State) => state.dogCharacteristics);
+    const dogDetails = useSelector((state: State) => state.dogDetails);
+    const loginInfo = useSelector((state: State) => state.loginInformation);
     const [isImageChosen, setImageChosen] = React.useState(false);
     const [isCharacteristicsInput, setCharacteristicsInput] = React.useState(false);
+    const [isDetailsInput, setDetailsInput] = React.useState(false);
 
     React.useEffect(() => {
         setImageChosen(image !== "")
@@ -27,12 +35,83 @@ export default function AddDogStack({ navigation }: any) {
     React.useEffect(() => {
         setCharacteristicsInput(areValidCharacteristics(dogCharacteristics));
     },[dogCharacteristics])
+    React.useEffect(() => {
+        setDetailsInput(areValidDetails(dogDetails));
+    },[dogDetails])
    
+    function areValidDetails(ch: IDogDetails){
+        return (ch.location != initDogDetails.location);
+      }
     function areValidCharacteristics(ch: IDogCharacteristics){
         return (ch.name != initLostDogCharacteristics.name && ch.breed != initLostDogCharacteristics.breed && ch.color != initLostDogCharacteristics.color
           && ch.size != initLostDogCharacteristics.size && ch.hairLength != initLostDogCharacteristics.hairLength && ch.tailLength != initLostDogCharacteristics.hairLength 
           && ch.tailLength != initLostDogCharacteristics.tailLength && ch.earsType != initLostDogCharacteristics.earsType && ch.specialMark != initLostDogCharacteristics.specialMark
           && ch.behaviors != initLostDogCharacteristics.behaviors);
+      }
+      function onExit(){
+          store.dispatch(Actions.setImage(""));
+          store.dispatch(Actions.setDogCharacteristics(initLostDogCharacteristics));
+          store.dispatch(Actions.setDogDetails(initDogDetails));
+      }
+
+      /*
+      dateLost: Date | null;
+  isFound: boolean;
+  ownerId: number;*/
+
+  /*
+  id: number;
+  name: string;
+  breed: BreedTypes | "";
+  age: number;
+  pictureId: number;
+  hairLength: HairTypes | "";
+  color: ColorTypes | "";
+  size: SizeTypes | "";
+  earsType: EarsTypes | "";
+  tailLength: TailTypes | "";
+  specialMark: SpecialMarkTypes | "";
+  behaviors: BehaviorsTypes[];
+  location: { city: string; district: string };*/
+
+  function str2ab(str: string) {
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
+      function publishDog(){
+          
+          store.dispatch(Actions.addDogThunk({
+            dog: {
+                ownerId: loginInfo?.id,
+                isFound: false,
+                dateLost: new Date(dogDetails.dateLost),
+                name: dogCharacteristics.name,
+                breed: dogCharacteristics.breed,
+                age: dogCharacteristics.age,
+                hairLength: dogCharacteristics.hairLength,
+                color: dogCharacteristics.color,
+                size: dogCharacteristics.size,
+                earsType: dogCharacteristics.earsType,
+                tailLength: dogCharacteristics.tailLength,
+                specialMark: dogCharacteristics.specialMark,
+                behaviors: dogCharacteristics.behaviors,
+                location: dogDetails.location
+            },
+            picture: {
+                id: picture.id,
+                fileName: picture.fileName,
+                fileType: picture.fileType,
+                data: str2ab(picture.data)
+            },
+            cookies: loginInfo?.token,
+          }))
+        
+        //onExit();
       }
 
   return (
@@ -42,10 +121,10 @@ export default function AddDogStack({ navigation }: any) {
       <Stack.Screen name="Choose photo" component={ImageUpload} 
       options={({ navigation }) => ({
         headerLeft: () => (
-          <Exit/>
+          <Exit onPress={() => onExit()}/>
         ),
         headerRight: () => (
-            <Next imageChosen={isImageChosen} onPress={() => navigation.navigate("Dog characteristics")}/>
+            <Next enabled={isImageChosen} onPress={() => navigation.navigate("Dog characteristics")}/>
         )
       })}
       />
@@ -55,7 +134,17 @@ export default function AddDogStack({ navigation }: any) {
             <Previous onPress={() => navigation.pop()}/>
           ),
           headerRight: () => (
-            <Next imageChosen={isCharacteristicsInput} onPress={() => navigation.navigate("Location and date")}/>
+            <Next enabled={isCharacteristicsInput} onPress={() => navigation.navigate("Review and post")}/>
+        )
+      })}
+      />
+      <Stack.Screen name="Review and post" component={ReviewAndPost} 
+      options={({navigation}) => ({
+        headerLeft: () => (
+            <Previous onPress={() => navigation.pop()}/>
+          ),
+          headerRight: () => (
+            <Post enabled={isDetailsInput} onPress={() => {publishDog(); }}/>
         )
       })}
       />
@@ -63,10 +152,10 @@ export default function AddDogStack({ navigation }: any) {
   );
 }
 
-function Exit(){
+function Exit(props: any){
     return(
         <View>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={props.onPress}>
                 <FontAwesomeIcon icon={faTimes} size={25} ></FontAwesomeIcon>
             </TouchableOpacity>
         </View>
@@ -87,8 +176,21 @@ const Next = (props: any) => {
 
     return(
         <View>
-            <TouchableOpacity disabled={props.isImageChosen} onPress={props.onPress}>
-                <FontAwesomeIcon icon={faChevronRight} size={25} color={props.imageChosen ? activeColor : inactiveColor}></FontAwesomeIcon>
+            <TouchableOpacity disabled={!(props.enabled)} onPress={props.onPress}>
+                <FontAwesomeIcon icon={faChevronRight} size={25} color={props.enabled ? activeColor : inactiveColor}></FontAwesomeIcon>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const Post= (props: any) => {
+
+    return(
+        <View >
+            <TouchableOpacity disabled={!(props.enabled)} onPress={props.onPress}>
+            <Text style={props.enabled ? { fontSize: 16, color: activeColor} : { fontSize: 15, color: inactiveColor}}>Post</Text>
+
+                {/* <FontAwesomeIcon icon={faChevronRight} size={25} color={props.enabled ? activeColor : inactiveColor}></FontAwesomeIcon> */}
             </TouchableOpacity>
         </View>
     );
