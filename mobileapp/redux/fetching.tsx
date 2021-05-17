ed52a73ef9ae1b26bd863139ff46ebd6ffe1e756
Polina Brazhko <polina.brazhko@gmail.com>
2021-05-17 12:13:59 +0200

    fetching (in progress)

--- mobileapp/redux/fetching.tsx
+++ mobileapp/redux/fetching.tsx
@@ -6,7 +6,7 @@ import {
   ILoginResults,
 } from "../components/loginRegisterInterfaces";
 import { APIResponse, RequestResponse } from "./response";
-import { ILostDogWithPicture } from "../components/dogs/dog/dogInterfaces";
+import { ILostDog, ILostDogWithPicture, IPicture } from "../components/dogs/dog/dogInterfaces";
 import { IRegisterRegularUserInformation } from "../components/register-login/loginRegisterInterfaces";
 
 async function getResponse<T>(
@@ -14,10 +14,12 @@ async function getResponse<T>(
 ): Promise<RequestResponse<T>> {
   try {
     const response = await axiosRequest;
+    console.log(response.status);
     return {
       code: response.status,
       response: response.data as APIResponse<T>,
     };
+
   } catch (error) {
     if (error instanceof TypeError || error.message === "Network Error") {
       return {
@@ -117,6 +119,103 @@ export async function registerRegularUser(
       formData,
       {
         headers: {
+          Accept: "application/json",
+        },
+      }
+    )
+  );
+}
+
+export async function addDog(
+  dog: ILostDog,
+  picture: IPicture,
+  Authorization: string
+): Promise<RequestResponse<ILostDogWithPicture>> {
+  let formData = new FormData();
+
+  // const privateProperties = ["id", "pictureId", "ownerId"];
+  // const excludePrivateProperties = (key: string, value: any) =>
+  //   privateProperties.includes(key) ? undefined : value;
+
+    /*id: 0,
+  ownerId: 0,
+  pictureId: 0,
+  name: "",
+  breed: "",
+  age: 0,
+  hairLength: "",
+  color: "",
+  size: "",
+  earsType: "",
+  tailLength: "",
+  specialMark: "",
+  behaviors: [],
+  location: { city: "", district: "" },
+  dateLost: new Date(2012, 1, 1, 0, 0, 0, 0),
+  isFound: false, */
+  
+  //formData.append("dog[id]", dog.id.toString());
+  //formData.append("dog[ownerId]",dog.ownerId.toString());
+  //formData.append("dog[pictureId]",dog.pictureId.toString());
+  console.log(JSON.stringify(dog.breed));
+  console.log(dog.breed.toString());
+  formData.append("dog[name]",JSON.stringify(dog.name));
+  formData.append("dog[breed]",JSON.stringify(dog.breed));
+  formData.append("dog[age]",JSON.stringify(dog.age));
+  formData.append("dog[hairLength]",JSON.stringify(dog.hairLength));
+  formData.append("dog[color]",JSON.stringify(dog.color));
+  formData.append("dog[size]",JSON.stringify(dog.size));
+  formData.append("dog[earsType]",JSON.stringify(dog.earsType));
+  formData.append("dog[tailLength]",JSON.stringify(dog.tailLength));
+  formData.append("dog[specialMark]",JSON.stringify(dog.specialMark));
+  formData.append("dog[behaviors]",JSON.stringify(dog.behaviors));
+  formData.append("dog[location[city]]",JSON.stringify(dog.location.city));
+  formData.append("dog[location[district]]",JSON.stringify(dog.location.district));
+  formData.append("dog[dateLost]",JSON.stringify(dog.dateLost));
+  formData.append("dog[isFound]",JSON.stringify(dog.isFound));
+
+  formData.append("picture[id]",JSON.stringify(picture.id));
+  formData.append("picture[fileName]",JSON.stringify(picture.fileName));
+  formData.append("picture[fileType]",JSON.stringify(picture.fileType));
+  formData.append("picture[data]",JSON.stringify(picture.data));
+  console.log("fetch!");
+  // formData.append('dog', {
+  //   "string": JSON.stringify(dog), //This is how it works :)
+  //   type: 'application/json'
+  // });
+
+  // formData.append("picture", {
+  //     id: picture.id,
+  //     fileName: picture.fileName,
+  //     fileType: picture.fileType,
+  //     data: picture.data
+  //   });
+
+
+
+  //formData.append("dog",{"string": JSON.stringify(dog), type: "application/json"})
+  // formData.append(
+  //   "dog",
+  //   new Blob([JSON.stringify(dog, excludePrivateProperties)], {
+  //     type: "application/json",
+  //   }),
+  //   ""
+  // );
+  console.log("formData:"+ formData.get("dog"));
+  // formData.append(
+  //   "picture",
+  //   new Blob([picture.data], { type: picture.fileType }),
+  //   picture.fileName
+  // );
+  console.log("formData:"+ formData.get("picture"));
+
+  return getResponse(
+    axios.post(
+      `http://192.168.1.15:${config.backend.port}/lostdogs`,
+      formData,
+      {
+        headers: {
+          Authorization: Authorization,
           Accept: "application/json",
         },
       }
