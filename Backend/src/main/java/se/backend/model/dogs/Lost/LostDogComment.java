3197b61bc91f70fefca595e2901e329f92140143
Andrzej Litkowski <lank891@gmail.com>
2021-05-30 19:16:45 +0200

    Tests for comments in service

--- Backend/src/main/java/se/backend/model/dogs/Lost/LostDogComment.java
+++ Backend/src/main/java/se/backend/model/dogs/Lost/LostDogComment.java
@@ -1,6 +1,7 @@
 package se.backend.model.dogs.Lost;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import se.backend.model.Location;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "lost_dog_comment")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
